55073afcf0aeff9a260839bcd4902070fe9e3b7f
ejafer <HolyJackDev@gmail.com>
2023-07-24 16:36:48 +0400

    Changed database to PostgreSQL

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,26 @@
+const HomePage = () => {
+  return (
+    <>
+      <h1 >Welcome to our Bike Shop!</h1>
+      <h1>Your One-Stop Destination for Cycling Enthusiasts!</h1>
+      <p>
+        At BikeShop.com, we are passionate about two things - bikes and
+        providing an exceptional shopping experience for all cycling enthusiasts
+        out there. Whether you're a seasoned rider, a weekend warrior, or just
+        discovering the joy of biking, we've got everything you need to elevate
+        your cycling journey.
+      </p>
+      <p>
+        Our motto is simple - "Ride with Passion, Shop with Confidence." We
+        believe that biking is not just a sport or a mode of transportation;
+        it's a way of life, a form of freedom, and a source of endless
+        adventure. That's why we strive to be more than just an ordinary
+        ecommerce shop. We aim to be your trusted companion, offering top-notch
+        products and outstanding service that will make your biking experience
+        truly remarkable.
+      </p>
+    </>
+  );
+};
+
+export default HomePage;
